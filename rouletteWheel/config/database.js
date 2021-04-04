2e998294a6f539a6de983948f79058f69d87e752
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-03 22:53:42 -0400

    init commit

---
+++ rouletteWheel/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Kso-savage:Ortega@cluster0.f93jb.mongodb.net/roulette?retryWrites=true&w=majority',
+  'dbName': 'roulette'
+};
